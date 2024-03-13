3772ab4da54da5a6853dceffab7a9d42040871b8
ivansukhoparov <rbcdaaa@gmail.com>
2024-03-13 01:00:48 +0100

    add: create common types

---
+++ src/features/common/types.ts
@@ -0,0 +1,47 @@
+import { Request } from 'express';
+
+export type RequestWithParams<P> = Request<P, {}, {}, {}>;
+export type RequestWithBody<B> = Request<{}, {}, B, {}>;
+export type RequestWithBodyAndParams<P, B> = Request<P, {}, B, {}>;
+export type RequestWithSearchTerms<T> = Request<{}, {}, {}, T>;
+export type RequestWithSearchTermsAndParams<P, T> = Request<P, {}, {}, T>;
+
+export type Params = {
+  id: string;
+};
+
+export type ErrorType = {
+  errorsMessages: ErrorsMessageType[];
+};
+export type ErrorsMessageType = {
+  field: string;
+  message: string;
+};
+
+export type QueryRequestType = {
+  searchLoginTerm?: string | null;
+  searchEmailTerm?: string | null;
+  searchNameTerm?: string | null;
+  sortBy?: string;
+  sortDirection?: 'asc' | 'desc';
+  pageNumber?: number;
+  pageSize?: number;
+};
+
+export type QuerySortType = {
+  sortBy: string;
+  sortDirection: 'asc' | 'desc';
+  pageNumber: number;
+  pageSize: number;
+};
+
+export type QuerySearchType = {
+  searchLoginTerm?: string | null;
+  searchEmailTerm?: string | null;
+  searchNameTerm?: string | null;
+};
+
+export type QueryDto = {
+  sortData: QuerySortType;
+  searchData: QuerySearchType;
+};
