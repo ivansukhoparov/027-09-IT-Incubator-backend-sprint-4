35243f218d7d49e58caae60205745543b6041b81
ivansukhoparov <rbcdaaa@gmail.com>
2024-03-24 21:44:34 +0100

    add: create JwtToken class

---
+++ src/common/jwt-tokens.ts
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtToken {
+  create = (payload: object, options: object, secretKey: string) => {
+    return jwt.sign(payload, secretKey, options);
+  };
+
+  verify = (token: string, secretKey: string) => {
+    try {
+      jwt.verify(token, secretKey);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  };
+
+  decode = (token: string) => {
+    return jwt.decode(token);
+  };
+}
