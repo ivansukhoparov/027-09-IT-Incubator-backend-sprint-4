81a8f8812c9035a1afdb41697c30c295e7b63dd1
ivansukhoparov <rbcdaaa@gmail.com>
2024-03-26 22:31:49 +0100

    fix: fix JwtToken Adapter

--- src/common/adapters/jwt.token.adapter.ts
+++ src/common/adapters/jwt.token.adapter.ts
@@ -3,20 +3,20 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class JwtTokenAdapter {
-  create = (payload: object, options: object, secretKey: string) => {
+  create(payload: object, options: object, secretKey: string) {
     return jwt.sign(payload, secretKey, options);
-  };
+  }
 
-  verify = (token: string, secretKey: string) => {
+  verify(token: string, secretKey: string) {
     try {
       jwt.verify(token, secretKey);
       return true;
     } catch (err) {
       return false;
     }
-  };
+  }
 
-  decode = (token: string) => {
+  decode(token: string) {
     return jwt.decode(token);
-  };
+  }
 }
