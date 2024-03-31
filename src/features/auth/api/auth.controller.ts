1cb8721ae01a9f1e289c1e52fb41127525e38d66
ivansukhoparov <rbcdaaa@gmail.com>
2024-03-31 23:22:58 +0200

    add: create registration confirmation and resending confirmation code flows

--- src/features/auth/api/auth.controller.ts
+++ src/features/auth/api/auth.controller.ts
@@ -1,5 +1,9 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { UserRegistrationDto } from '../types/input';
+import {
+  UserConfirmationCodeDto,
+  UserEmailDto,
+  UserRegistrationDto,
+} from '../types/input';
 import { AuthService } from '../application/auth.service';
 
 @Controller('auth')
@@ -7,9 +11,26 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('registration')
-  @HttpCode(HttpStatus.CREATED)
+  @HttpCode(HttpStatus.NO_CONTENT)
   async registration(@Body() registrationDto: UserRegistrationDto) {
     const isSuccess = await this.authService.registerUser(registrationDto);
+    if (isSuccess) return;
+  }
+
+  @Post('registration-confirmation')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async registrationConfirmation(
+    @Body() confirmationCode: UserConfirmationCodeDto,
+  ) {
+    const isSuccess = await this.authService.confirmEmail(confirmationCode);
+    if (isSuccess) return;
+  }
+
+  @Post('registration-email-resending')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async registrationEmailResending(@Body() resendingRequestDto: UserEmailDto) {
+    const isSuccess =
+      await this.authService.resendConfirmationCode(resendingRequestDto);
     if (isSuccess) return;
   }
 }
--- src/features/auth/application/auth.service.ts
+++ src/features/auth/application/auth.service.ts
@@ -1,28 +1,63 @@
-import { UserRegistrationDto } from '../types/input';
+import {
+  UserConfirmationCodeDto,
+  UserEmailDto,
+  UserRegistrationDto,
+} from '../types/input';
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../../users/application/users.service';
 import { EmailConfirmationCode } from '../../../common/email.confirmation.code';
+import { EmailService } from '../../../common/email/email.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UsersService,
     private readonly emailConfirmationCode: EmailConfirmationCode,
+    private readonly emailService: EmailService,
   ) {}
 
   async registerUser(registrationDto: UserRegistrationDto) {
     const createdUserId = await this.userService.create(registrationDto, false);
     const createdUser = await this.userService.getUserById(createdUserId);
     if (!createdUser) return false;
-    const confirmationCode = this.emailConfirmationCode.create(
-      createdUser.email,
+    this.emailConfirmationCode.create({ email: createdUser.email });
+    const isEmailSent = await this.emailService.sendEmailConfirmationEmail(
+      createdUser,
+      this.emailConfirmationCode.get(),
+    );
+    if (!isEmailSent) {
+      await this.userService.delete(createdUser.id);
+      return false;
+    }
+    return true;
+  }
+
+  async resendConfirmationCode(email: UserEmailDto) {
+    const user = await this.userService.getUserByLoginOrEmail(email.email);
+    if (!user) return false;
+    if (user.isConfirmed) return false;
+
+    this.emailConfirmationCode.create(email);
+
+    const isEmailSent = await this.emailService.reSendEmailConfirmationEmail(
+      user,
+      this.emailConfirmationCode.get(),
+    );
+    return true;
+  }
+
+  async confirmEmail(confirmationCode: UserConfirmationCodeDto) {
+    this.emailConfirmationCode.set(confirmationCode.code);
+
+    if (!this.emailConfirmationCode.verify()) return false;
+
+    const user = await this.userService.getUserByLoginOrEmail(
+      this.emailConfirmationCode.decode().email,
     );
 
-    // const isEmailSent = await EmailAdapter.sendEmailConfirmationEmail(createdUser);
-    // if (!isEmailSent) {
-    //     await this.usersRepository.deleteUser(createdUser.id);
-    //     return false;
-    // }
-    return true;
+    if (!user) return false;
+    if (user.isConfirmed) return false;
+
+    return await this.userService.updateUserConfirmationStatus(user.id);
   }
 }
