0c9e04f804702767a789ae31c2348f7d67212ba7
ivansukhoparov <rbcdaaa@gmail.com>
2024-03-31 23:19:18 +0200

    add:  create updateUserConfirmationStatus method at user service

--- src/features/users/application/users.service.ts
+++ src/features/users/application/users.service.ts
@@ -25,15 +25,21 @@ export class UsersService {
     return await this.usersRepository.createUser(newUser);
   }
 
-  async delete(id: string) {
-    await this.usersRepository.deleteUser(id);
-  }
-
   async getUserByLoginOrEmail(loginOrEmail: string) {
     return this.usersRepository.getUserByLoginOrEmail(loginOrEmail);
   }
 
   async getUserById(id: string) {
     return this.usersRepository.getUserById(id);
+  }
+
+  async updateUserConfirmationStatus(id: string) {
+    const userUpdateDto = { isConfirmed: true };
+    const result = await this.usersRepository.updateUser(id, userUpdateDto);
+    return result;
+  }
+
+  async delete(id: string) {
+    await this.usersRepository.deleteUser(id);
   }
 }
