1771c94aeca1037049ccb4508e3265f7391b494f
ivansukhoparov <rbcdaaa@gmail.com>
2024-04-01 03:06:46 +0200

    add: create login method in service

--- src/features/auth/application/auth.service.ts
+++ src/features/auth/application/auth.service.ts
@@ -1,12 +1,16 @@
 import {
   UserConfirmationCodeDto,
   UserEmailDto,
+  UserLoginDto,
   UserRegistrationDto,
 } from '../types/input';
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../../users/application/users.service';
 import { EmailConfirmationCode } from '../../../common/email.confirmation.code';
 import { EmailService } from '../../../common/email/email.service';
+import { BcryptAdapter } from '../../../common/adapters/bcrypt.adapter';
+import { AccessToken } from '../../../common/access.token';
+import { RefreshToken } from '../../../common/refresh.token';
 
 @Injectable()
 export class AuthService {
@@ -14,6 +18,9 @@ export class AuthService {
     private readonly userService: UsersService,
     private readonly emailConfirmationCode: EmailConfirmationCode,
     private readonly emailService: EmailService,
+    private readonly cryptAdapter: BcryptAdapter,
+    private readonly accessToken: AccessToken,
+    private readonly refreshToken: RefreshToken,
   ) {}
 
   async registerUser(registrationDto: UserRegistrationDto) {
@@ -39,11 +46,10 @@ export class AuthService {
 
     this.emailConfirmationCode.create(email);
 
-    const isEmailSent = await this.emailService.reSendEmailConfirmationEmail(
+    return await this.emailService.reSendEmailConfirmationEmail(
       user,
       this.emailConfirmationCode.get(),
     );
-    return true;
   }
 
   async confirmEmail(confirmationCode: UserConfirmationCodeDto) {
@@ -59,5 +65,39 @@ export class AuthService {
     if (user.isConfirmed) return false;
 
     return await this.userService.updateUserConfirmationStatus(user.id);
+  }
+
+  async loginUser(loginDto: UserLoginDto) {
+    const user = await this.userService.getUserByLoginOrEmail(
+      loginDto.loginOrEmail,
+    );
+    if (!user) return null;
+
+    const isSuccess = await this.cryptAdapter.compareHash(
+      loginDto.password,
+      user.hash,
+    );
+    if (!isSuccess) return null;
+
+    const deviceId = '100'; //uuidv4();
+
+    this.accessToken.create({ userId: user._id.toString() });
+    this.refreshToken.create({
+      userId: user._id.toString(),
+      deviceId: deviceId,
+    });
+
+    const tokens = {
+      accessToken: this.accessToken.get(),
+      refreshToken: this.refreshToken.get(),
+    };
+
+    // const sessionIsCreate = await this.securityService.createAuthSession(
+    //   tokens.refreshToken,
+    //   deviceTitle,
+    //   ip,
+    // );
+    // if (!sessionIsCreate) return null;
+    return tokens;
   }
 }
