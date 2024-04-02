e1fe2f5defbef643f3c90bf908d2f073eb7aca1a
ivansukhoparov <rbcdaaa@gmail.com>
2024-04-03 00:25:27 +0200

    refactoring: rebuilt password.recovery.token.service with abstrat base class

---
+++ src/common/token.services/password.recovery.token.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { appSettings } from '../../settings/app.settings';
+import { JwtPayload } from 'jsonwebtoken';
+import { BaseToken } from '../../base/base.classes/base.token';
+import {
+  PasswordRecoveryTokenDecodedDto,
+  PasswordRecoveryTokenPayloadDto,
+} from './types/password.recovery.token';
+
+@Injectable()
+export class PasswordRecoveryTokenService extends BaseToken<
+  PasswordRecoveryTokenPayloadDto,
+  PasswordRecoveryTokenDecodedDto
+> {
+  constructor() {
+    super(
+      appSettings.api.JWT_SECRET_KEY,
+      appSettings.api.RECOVERY_TOKEN_EXPIRATION_TIME,
+    );
+  }
+
+  tokenMapper(decodedToken: JwtPayload): PasswordRecoveryTokenDecodedDto {
+    return {
+      userId: decodedToken.userId,
+      iat: decodedToken.iat,
+      exp: decodedToken.exp,
+    };
+  }
+}
