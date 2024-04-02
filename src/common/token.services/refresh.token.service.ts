e07ddee6fef03b19b0df5951236a57dc5794fade
ivansukhoparov <rbcdaaa@gmail.com>
2024-04-03 00:25:04 +0200

    refactoring: rebuilt refresh.token.service with abstrat base class

---
+++ src/common/token.services/refresh.token.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { appSettings } from '../../settings/app.settings';
+import { JwtPayload } from 'jsonwebtoken';
+import { BaseToken } from '../../base/base.classes/base.token';
+import {
+  RefreshTokenDecodedDto,
+  RefreshTokenPayloadDto,
+} from './types/refresh.token';
+
+@Injectable()
+export class RefreshTokenService extends BaseToken<
+  RefreshTokenPayloadDto,
+  RefreshTokenDecodedDto
+> {
+  constructor() {
+    super(
+      appSettings.api.JWT_SECRET_KEY,
+      appSettings.api.RECOVERY_TOKEN_EXPIRATION_TIME,
+    );
+  }
+
+  tokenMapper(decodedToken: JwtPayload): RefreshTokenDecodedDto {
+    return {
+      userId: decodedToken.userId,
+      deviceId: decodedToken.deviceId,
+      iat: decodedToken.iat,
+      exp: decodedToken.exp,
+    };
+  }
+}
