3a305194eee5e0abe1150f9d8e946e4edacb9c15
ivansukhoparov <rbcdaaa@gmail.com>
2024-04-04 22:30:17 +0200

    add: add transform:true to ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {ValidationPipe} from '@nestjs/common';
+import {HttpExceptionFilter} from "./infrastructure/exception-filters/http.exception.filter";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe({
+        transform:true,
+    }));
+    app.useGlobalFilters(new HttpExceptionFilter());
+    await app.listen(3000);
 }
+
 bootstrap();
