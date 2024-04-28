550fda5675fbefd9ed8f30dc2f2d6997c18fdfa5
ivansukhoparov <rbcdaaa@gmail.com>
2024-04-28 21:59:08 +0200

    add: create add comments routs

--- src/features/comments/api/comments.controller.ts
+++ src/features/comments/api/comments.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Put, UseGuards } from '@nestjs/common';
 import { CommentsQueryRepository } from '../infrastructure/comments.query.repository';
+import { AuthGuard } from '../../../infrastructure/guards/admin-auth-guard.service';
 
 @Controller('comments')
 export class CommentsController {
@@ -9,4 +10,16 @@ export class CommentsController {
   async getById(@Param('id') id: string) {
     return this.commentsQueryRepository.getById(id);
   }
+
+  @Put(':id')
+  @UseGuards(AuthGuard)
+  async updateComment() {}
+
+  @Delete()
+  @UseGuards(AuthGuard)
+  async deleteComment() {}
+
+  @Put(':id/like-status')
+  @UseGuards(AuthGuard)
+  async updateLikeStatus() {}
 }
