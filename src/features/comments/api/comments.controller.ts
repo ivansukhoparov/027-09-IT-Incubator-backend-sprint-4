0160fde67e5e4f1ae2cc0ea643d96847fa896606
ivansukhoparov <rbcdaaa@gmail.com>
2024-05-01 01:19:16 +0200

    add: add guard to comments controller

--- src/features/comments/api/comments.controller.ts
+++ src/features/comments/api/comments.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Delete, Get, Param, Put, UseGuards } from '@nestjs/common';
+import {Controller, Delete, Get, Param, Put, UseGuards, Post, Req} from '@nestjs/common';
 import { CommentsQueryRepository } from '../infrastructure/comments.query.repository';
-import { AuthGuard } from '../../../infrastructure/guards/admin-auth-guard.service';
+import {AuthGuard, SoftAuthGuard} from '../../../infrastructure/guards/admin-auth-guard.service';
+import {Request} from "express";
 
 @Controller('comments')
 export class CommentsController {
   constructor(protected commentsQueryRepository: CommentsQueryRepository) {}
 
   @Get(':id')
+  @UseGuards(SoftAuthGuard)
   async getById(@Param('id') id: string) {
     return this.commentsQueryRepository.getById(id);
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  async createComment(@Req() req: any,) {
+    return req.user
   }
 
   @Put(':id')
