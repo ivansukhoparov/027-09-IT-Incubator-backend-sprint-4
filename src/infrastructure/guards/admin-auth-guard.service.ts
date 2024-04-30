2ea1b273912cac086bc39e450e832fab844806c7
ivansukhoparov <rbcdaaa@gmail.com>
2024-05-01 01:17:35 +0200

    add: soft auth guard

--- src/infrastructure/guards/admin-auth-guard.service.ts
+++ src/infrastructure/guards/admin-auth-guard.service.ts
@@ -1,71 +1,105 @@
 import {
-  CanActivate,
-  ExecutionContext,
-  HttpException,
-  HttpStatus,
-  Injectable,
-  NotAcceptableException,
+    CanActivate,
+    ExecutionContext,
+    HttpException,
+    HttpStatus,
+    Injectable,
+    NotAcceptableException,
 } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { HttpExceptionFilter } from '../exception-filters/http.exception.filter';
-import { AccessTokenService } from '../../common/token.services/access.token.service';
-import { tokenServiceCommands } from '../../common/token.services/utils/common';
+import {Observable} from 'rxjs';
+import {HttpExceptionFilter} from '../exception-filters/http.exception.filter';
+import {AccessTokenService} from '../../common/token.services/access.token.service';
+import {tokenServiceCommands} from '../../common/token.services/utils/common';
+import {UsersService} from "../../features/users/application/users.service";
 
 export const AUTH_METHODS = {
-  base: 'Basic',
-  bearer: 'Bearer',
+    base: 'Basic',
+    bearer: 'Bearer',
 };
 
 @Injectable()
 export class AdminAuthGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    try {
-      const request = context.switchToHttp().getRequest();
-      const authHeader = request.header('authorization')?.split(' '); // Получаем значение поля в заголовке
-      const authMethod = authHeader[0]; // получаем метод из заголовка
-      const authInput = authHeader[1]; // получаем значение для авторизации из заголовка
-      const auth = btoa(`${'admin'}:${'qwerty'}`); // кодируем наши логин и пароль в basic64
+    canActivate(
+        context: ExecutionContext,
+    ): boolean | Promise<boolean> | Observable<boolean> {
+        try {
+            const request = context.switchToHttp().getRequest();
+            const authHeader = request.header('authorization')?.split(' '); // Получаем значение поля в заголовке
+            const authMethod = authHeader[0]; // получаем метод из заголовка
+            const authInput = authHeader[1]; // получаем значение для авторизации из заголовка
+            const auth = btoa(`${'admin'}:${'qwerty'}`); // кодируем наши логин и пароль в basic64
 
-      if (authInput === auth && authMethod === AUTH_METHODS.base) {
-        return true;
-      } else {
-        throw new HttpException(
-          'Bad login or password',
-          HttpStatus.UNAUTHORIZED,
-        );
-      }
-    } catch {
-      throw new HttpException('Bad login or password', HttpStatus.UNAUTHORIZED);
+            if (authInput === auth && authMethod === AUTH_METHODS.base) {
+                return true;
+            } else {
+                throw new HttpException(
+                    'Bad login or password',
+                    HttpStatus.UNAUTHORIZED,
+                );
+            }
+        } catch {
+            throw new HttpException('Bad login or password', HttpStatus.UNAUTHORIZED);
+        }
     }
-  }
 }
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    try {
-      const request = context.switchToHttp().getRequest();
-      const authHeader = request.header('authorization')?.split(' '); // Получаем значение поля в заголовке
-      const authMethod = authHeader[0]; // получаем метод из заголовка
-      const token = new AccessTokenService(
-        tokenServiceCommands.set,
-        authHeader[1],
-      );
 
-      if (token.verify() && authMethod === AUTH_METHODS.bearer) {
-        return true;
-      } else {
-        throw new HttpException(
-          'Bad login or password',
-          HttpStatus.UNAUTHORIZED,
-        );
-      }
-    } catch {
-      throw new HttpException('Bad login or password', HttpStatus.UNAUTHORIZED);
+    constructor(protected usersService: UsersService) {
     }
-  }
+
+    async canActivate(
+        context: ExecutionContext,
+    ): Promise<boolean> {
+        try {
+            const request = context.switchToHttp().getRequest();
+            const authHeader = request.header('authorization')?.split(' '); // Получаем значение поля в заголовке
+            const authMethod = authHeader[0]; // получаем метод из заголовка
+            const token = new AccessTokenService(
+                tokenServiceCommands.set,
+                authHeader[1],
+            );
+
+            if (token.verify() && authMethod === AUTH_METHODS.bearer) {
+                const userId = token.decode().userId;
+                request.user = await this.usersService.getUserById(userId)
+                return true;
+            } else {
+                throw new HttpException(
+                    'Bad login or password',
+                    HttpStatus.UNAUTHORIZED,
+                );
+            }
+        } catch {
+            throw new HttpException('Bad login or password', HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
+
+@Injectable()
+export class SoftAuthGuard implements CanActivate {
+
+    constructor(protected usersService: UsersService) {
+    }
+
+    async canActivate(
+        context: ExecutionContext,
+    ): Promise<boolean> {
+        try {
+            const request = context.switchToHttp().getRequest();
+            const authHeader = request.header('authorization')?.split(' '); // Получаем значение поля в заголовке
+            const authMethod = authHeader[0]; // получаем метод из заголовка
+            const token = new AccessTokenService(
+                tokenServiceCommands.set,
+                authHeader[1],
+            );
+
+            const userId = token.decode().userId;
+            request.user = await this.usersService.getUserById(userId)
+            return true;
+        } catch {
+            return true;
+        }
+    }
 }
