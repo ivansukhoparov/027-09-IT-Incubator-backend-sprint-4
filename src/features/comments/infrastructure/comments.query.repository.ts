8b1bbcd3498962d1cc1e74199e3c95ae65998be0
ivansukhoparov <rbcdaaa@gmail.com>
2024-05-01 01:19:58 +0200

    add: add comments service logic

--- src/features/comments/infrastructure/comments.query.repository.ts
+++ src/features/comments/infrastructure/comments.query.repository.ts
@@ -1,18 +1,34 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { CommentDocument, Comments } from './comments.schema';
-import { commentMapper } from '../types/mapper';
+import {BadGatewayException, Injectable, NotFoundException} from '@nestjs/common';
+import {InjectModel} from '@nestjs/mongoose';
+import {Model} from 'mongoose';
+import {CommentDocument, Comments} from './comments.schema';
+import {commentMapper} from '../types/mapper';
+import {LikesInfoType} from "../types/output";
+import {LikeStatusType} from "../types/input";
 
 @Injectable()
 export class CommentsQueryRepository {
-  constructor(
-    @InjectModel(Comments.name) private commentModel: Model<Comments>,
-  ) {}
+    constructor(
+        @InjectModel(Comments.name) private commentModel: Model<Comments>,
+    ) {
+    }
 
-  async getById(id: string) {
-    const comment: CommentDocument = await this.commentModel.findById(id);
-    if (!comment) throw new NotFoundException();
-    return commentMapper(comment);
-  }
+    async getById(id: string) {
+
+        try {
+            const comment: CommentDocument = await this.commentModel.findById(id);
+            //const likes:LikesInfoType = await this.getLikes(commentId, userId);
+            const likes: LikesInfoType = {
+                likesCount: 0,
+                dislikesCount: 0,
+                myStatus: "None"
+            }
+            const result = commentMapper(comment, likes);
+            if (comment) return result
+            else throw new NotFoundException();
+        } catch (err) {
+            throw new BadGatewayException();
+        }
+
+    }
 }
