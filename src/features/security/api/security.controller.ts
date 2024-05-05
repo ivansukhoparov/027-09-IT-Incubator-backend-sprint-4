682a34d7d52daed7817efd9bdf3cf9464bcf1f78
ivansukhoparov <rbcdaaa@gmail.com>
2024-05-05 22:59:49 +0200

    add: create security controller routers

--- src/features/security/api/security.controller.ts
+++ src/features/security/api/security.controller.ts
@@ -1,15 +1,50 @@
-import {Controller, Delete, Get, Param} from "@nestjs/common";
-
+import {
+    Controller,
+    Delete, ForbiddenException,
+    Get,
+    HttpCode,
+    HttpStatus,
+    NotFoundException,
+    Param,
+    Req,
+    UnauthorizedException
+} from "@nestjs/common";
+import {Request} from "express"
+import {SessionsQueryRepository} from "../infrastructure/sessions.query.repository";
+import {SessionsRepository} from "../infrastructure/sessions.repository";
+import {RefreshTokenService} from "../../../common/token.services/refresh.token.service";
+import {SessionsService} from "../application/sessions.service";
 
 @Controller("security")
-export class SecurityController{
+export class SecurityController {
+
+    constructor(protected sessionsQueryRepository: SessionsQueryRepository,
+                protected sessionsService: SessionsService,) {
+    }
 
     @Get("devices")
-    async getAllActiveSessions(){}
+    async getAllActiveSessions(@Req() req: Request) {
+        try {
+            return await this.sessionsQueryRepository.getSessionsByUserId(req.cookies.refreshToken)
+        } catch {
+            throw new UnauthorizedException()
+        }
+    }
 
     @Delete("devices")
-    async terminateAllActiveSessions(){}
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async terminateAllActiveSessions(@Req() req: Request) {
+        await this.sessionsService.terminateAllSessions(req.cookies.refreshToken)
+        return
+    }
 
     @Delete("devices/:id")
-    async terminateSessions(@Param() id:string){}
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async terminateSession(@Param("id") id: string, @Req() req: Request) {
+        const interlayerModel = await this.sessionsService.terminateSession(id, req.cookies.refreshToken)
+        if (!interlayerModel.hasError()) return
+        if (interlayerModel.code === 401) throw new UnauthorizedException()
+        if (interlayerModel.code === 404) throw new NotFoundException()
+        if (interlayerModel.code === 403) throw new ForbiddenException()
+    }
 }
