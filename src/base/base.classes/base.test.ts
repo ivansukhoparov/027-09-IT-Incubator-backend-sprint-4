a0f745d905bebe1f0f4439f1dabbd0e199a11c7d
ivansukhoparov <rbcdaaa@gmail.com>
2024-05-07 21:30:22 +0200

    refactoring: delete something

--- src/base/base.classes/base.test.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthService } from '../../features/auth/application/auth.service';
-import { UsersService } from '../../features/users/application/users.service';
-import { UsersQueryRepository } from '../../features/users/infrastructure/users.query.repository';
-
-@Injectable()
-export class TestBaseClass {
-  constructor(protected users: UsersQueryRepository) {}
-}
